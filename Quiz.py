9a369e83bfa026a107bc17bfa7a081551d8562d2
harshbhimra01 <honey.computer0000@gmail.com>
2024-06-21 12:17:32 +0530

    Initial commit

--- Quiz.py
+++ Quiz.py
@@ -8,36 +8,36 @@ if playing.lower() != "yes":
 print("Okay! Let's play 😄")
 score = 0
 
-answer = input("What is phone? ")
-if answer.lower() == ('a rectangular device'):
+answer = input("What is a computer? ")
+if answer.lower() == ('device'):
     print('Correct!')
     score += 1
 else:
     print('Incorrect')
 
-answer = input("What is pc? ")
-if answer.lower() == ('personal computer'):
+answer = input("What are the main components of a computer system? ")
+if answer.lower() == ('hardware'):
     print('Correct!')
     score += 1
 else:
     print('Incorrect')
 
-answer = input("What is laptop? ")
-if answer.lower() == ('computer alternative'):
+answer = input("What is an operating system? ")
+if answer.lower() == ('software'):
     print('Correct!')
     score += 1
 else:
     print('Incorrect')
 
-answer = input("What is water? ")
-if answer.lower() == ('liquid'):
+answer = input("What is the function of cpu? ")
+if answer.lower() == ('processing'):
     print('Correct!')
     score += 1
 else:
     print('Incorrect')
 
-answer = input("What is human? ")
-if answer.lower() == ('trash'):
+answer = input("What is the difference between RAM and ROM? ")
+if answer.lower() == ('volatility'):
     print('Correct!')
     score += 1
 else:
